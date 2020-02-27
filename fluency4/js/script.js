8622874f5d16a22ec1a69945955537bf98b87ad8
Sergey Herman <HermanSP@corp.oakmgmt.ru>
2020-02-27 16:42:48 +0300

    ajax added

---
+++ fluency4/js/script.js
@@ -0,0 +1,28 @@
+var templeList = document.getElementById("fluency4");
+
+var btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+var ourRequest = new XMLHttpRequest();
+
+ourRequest.open('GET', 'https://sergeherman.github.io/siarheiherman.github.io/fluency4/towndata.json');	
+ourRequest.onload = function() {
+	
+ var ourData = JSON.parse(ourRequest.responseText);
+	renderHTML(ourData);
+};
+ourRequest.send();	
+});
+
+function renderHTML(data) {
+	var htmlString = "";
+	
+	for (i = 0; i < data.length; i++) {
+	//	htmlString += "<p>" + data[i].name + "is a" + data[i].species + ".</p>";
+		htmlString += "<p>" + data[i].name + ".</p>";
+	}
+ templeList.insertAdjacentHTML('beforeend', htmlString);
+	
+}
+
+
