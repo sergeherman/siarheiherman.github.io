8c2e4c336711d4018c70ad4ae47613690034f916
Sergey Herman <HermanSP@corp.oakmgmt.ru>
2020-03-03 18:08:05 +0300

    css js manipulation added

---
+++ fluency7/js/script.js
@@ -0,0 +1,12 @@
+function changeColor() {
+
+document.getElementById("fluency7").style.color = "yellow";
+document.getElementById("fluency7").style.fontSize = "30px";
+}
+
+function changeBackground() {
+
+document.getElementById("fluency7a").style.backgroundColor = "red";
+document.getElementById("fluency7a").style.fontSize = "30px";
+}
+
