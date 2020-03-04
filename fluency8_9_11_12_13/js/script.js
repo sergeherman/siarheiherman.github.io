46add8aed42f9205d32040d5bbf80b1fdd553375
Sergey Herman <HermanSP@corp.oakmgmt.ru>
2020-03-04 17:32:10 +0300

    added css3 transitions animations transforms

---
+++ fluency8_9_11_12_13/js/script.js
@@ -0,0 +1,8 @@
+function changeBG(element, color) {
+	var element = document.getElementById(element);
+	element.style.transition = "background 1.0s ease-in 0s";
+	element.style.background = color;
+	
+	
+	
+}
